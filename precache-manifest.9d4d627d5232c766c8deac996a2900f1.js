20051413316bd0db57ab3bcbe92809142f805055
wicstily <wicstily@gmail.com>
2019-02-17 00:36:58 +0400

    Updates

---
+++ precache-manifest.9d4d627d5232c766c8deac996a2900f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "cfe963e3612fe255984f",
+    "url": "/contactmanager/static/js/main.cfe963e3.chunk.js"
+  },
+  {
+    "revision": "bdad147c60e34a544ee5",
+    "url": "/contactmanager/static/js/2.bdad147c.chunk.js"
+  },
+  {
+    "revision": "cfe963e3612fe255984f",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "8fb10258a5db99562f392b8030931152",
+    "url": "/contactmanager/index.html"
+  }
+];
